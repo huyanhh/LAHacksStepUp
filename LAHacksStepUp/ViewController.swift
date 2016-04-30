91b4b6fbbb77cb4bf709c5981181d88a4be75b64
Huyanh Hoang <huyanhh@uci.edu>
2016-04-30 01:05:28 -0700

    Initial Commit

---
+++ LAHacksStepUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LAHacksStepUp
+//
+//  Created by Huyanh Hoang on 2016. 4. 30..
+//  Copyright © 2016년 LAHacksTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
